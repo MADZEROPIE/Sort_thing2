ce8e1d4968a4b8bff9c4c3388ce93bb91e733ba1
MaDZeRoPiE <sanich2000qq@gmail.com>
2019-11-04 20:53:47 +0300

    quick sort with tests

--- samples/sample_sort.cpp
+++ samples/sample_sort.cpp
@@ -4,7 +4,15 @@
 
 int main(int argc, char** argv)
 {
+	srand(0);
 	setlocale(LC_ALL, "Russian");
+	int arr[6];
+	for (int i = 1; i < 7; ++i) arr[i-1] = rand();
+	for (int i = 0; i < 6; ++i) std::cout << arr[i] << " ";
+	std::cout << std::endl;
+	quick_sort(arr, 6);
+	for (int i = 0; i < 6; ++i) std::cout << arr[i] << " ";
+	std::cout << std::endl;
 	return 0;
 }
 
