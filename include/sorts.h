9589b5d0afc9a1e8cce12480d0665f2d3c9dbe68
MaDZeRoPiE <sanich2000qq@gmail.com>
2019-11-14 22:50:31 +0300

    fixes

--- include/sorts.h
+++ include/sorts.h
@@ -44,32 +44,8 @@ void quick_sort2(int arr[], int right) {//Версия, при которой выбирается самый л
 }
 */
 
-int get_max(int a[], int n) {
-	int m = a[0];
-	for (int i = 1; i < n; i++)
-		if (a[i] > m)
-			m = a[i];
-	return m;
-}
+int get_max(int a[], int n);
 
-void count_sort(int a[], int n, int num) {
-	int* temp_arr = (int*)malloc(n * sizeof(a[0]));
-	int i;
-	int count[10] = { 0 };
-	for (i = 0; i < n; i++)
-		count[(a[i] / num) % 10]++;
-	for (i = 1; i < 10; i++)
-		count[i] += count[i - 1];
-	for (i = n - 1; i >= 0; i--) {
-		temp_arr[count[(a[i] / num) % 10] - 1] = a[i];
-		count[(a[i] / num) % 10]--;
-	}
-	for (i = 0; i < n; i++)
-		a[i] = temp_arr[i];
-}
+void count_sort(int a[], int n, int num);
 
-void radix_sort(int a[], int n) { //Копипаста оттуда же
-	int m = get_max(a, n);
-	for (int num = 1; m / num > 0; num *= 10)
-		count_sort(a, n, num);
-}
+void radix_sort(int a[], int n);
--- src/sorts.cpp
+++ src/sorts.cpp
@@ -1,4 +1,39 @@
-﻿//#include "sorts.h"
+﻿#include "..\include\sorts.h"
+//#include "sorts.h"
 //#include <iostream>
 //#include <cstdlib>
 //#include <ctime>
+
+int get_max(int a[], int n)
+{
+	int m = a[0];
+	for (int i = 1; i < n; i++)
+		if (a[i] > m)
+			m = a[i];
+	return m;
+}
+
+void count_sort(int a[], int n, int num)
+{
+	int* temp_arr = (int*)malloc(n * sizeof(a[0]));
+	int i;
+	int count[10] = { 0 };
+	for (i = 0; i < n; i++)
+		count[(a[i] / num) % 10]++;
+	for (i = 1; i < 10; i++)
+		count[i] += count[i - 1];
+	for (i = n - 1; i >= 0; i--) {
+		temp_arr[count[(a[i] / num) % 10] - 1] = a[i];
+		count[(a[i] / num) % 10]--;
+	}
+	for (i = 0; i < n; i++)
+		a[i] = temp_arr[i];
+	free(temp_arr);
+}
+
+void radix_sort(int a[], int n)
+{
+	int m = get_max(a, n);
+	for (int num = 1; m / num > 0; num *= 10)
+		count_sort(a, n, num);
+}
